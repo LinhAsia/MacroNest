d30f1c72806e7b8e257044f218d8282aa524a219
ngbal <ngbal@example.com>
2026-06-20 17:40:00 +0700

    Change settings popup layer order to Foreground to prevent hiding nested ComboBox dropdown lists

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -4155,7 +4155,7 @@ impl CrosshairApp {
                 let pos = button_response.rect.left_bottom() + vec2(-42.0, 4.0);
                 let mut content_rect = egui::Rect::NOTHING;
                 let area_response = egui::Area::new(popup_id)
-                    .order(egui::Order::Tooltip)
+                    .order(egui::Order::Foreground)
                     .fixed_pos(pos)
                     .show(ui.ctx(), |ui| {
                         let frame_response = egui::Frame::popup(ui.style())
