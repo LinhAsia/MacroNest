02d390647f16d6538e39a813d62e3a2ceafcc8b2
ngbal <ngbal@example.com>
2026-06-30 14:34:26 +0700

    Adjust usagi mascot bounds and redraw tool icons

--- src/overlay/mod.rs
+++ src/overlay/mod.rs
@@ -7241,7 +7241,7 @@ mod windows_overlay {
         let scale = quick_key_display_mascot_scale(font_size, mascot_style);
         let (left, top, right, bottom) = match mascot_style {
             crate::model::MascotStyle::Hachiware => (38.0, 16.0, 88.0, 62.0),
-            crate::model::MascotStyle::ChiikawaClassic => (-28.0, 6.0, -28.0, 62.0),
+            crate::model::MascotStyle::ChiikawaClassic => (122.0, 6.0, 122.0, 62.0),
             crate::model::MascotStyle::Chiikawa => (40.0, 10.0, 88.0, 62.0),
         };
         (
@@ -11512,29 +11512,33 @@ mod windows_overlay {
             },
         );
         let brush_center_x = SCREEN_DRAW_TOOLBAR_BRUSH_X as f32 + 14.0;
-        draw_skia_line(
-            &mut pixmap,
-            brush_center_x - 6.5,
-            35.5,
-            brush_center_x + 4.5,
-            22.0,
-            [240, 246, 255, 246],
-            3.4,
-        );
-        draw_skia_circle_fill(
-            &mut pixmap,
-            brush_center_x - 5.8,
-            35.6,
-            2.6,
-            [218, 230, 246, 246],
-        );
-        let mut brush_tip = tiny_skia::PathBuilder::new();
-        brush_tip.move_to(brush_center_x + 1.2, 24.0);
-        brush_tip.line_to(brush_center_x + 8.2, 16.0);
-        brush_tip.line_to(brush_center_x + 4.8, 12.8);
-        brush_tip.line_to(brush_center_x - 1.0, 20.0);
-        brush_tip.close();
-        if let Some(path) = brush_tip.finish() {
+        let mut brush_handle = tiny_skia::PathBuilder::new();
+        brush_handle.move_to(brush_center_x - 7.0, 35.0);
+        brush_handle.line_to(brush_center_x - 3.0, 38.0);
+        brush_handle.line_to(brush_center_x + 4.5, 28.0);
+        brush_handle.line_to(brush_center_x + 0.2, 24.5);
+        brush_handle.close();
+        if let Some(path) = brush_handle.finish() {
+            fill_skia_path(&mut pixmap, &path, [212, 224, 242, 246]);
+            stroke_skia_path(&mut pixmap, &path, [240, 246, 255, 246], 1.4);
+        }
+        let mut ferrule = tiny_skia::PathBuilder::new();
+        ferrule.move_to(brush_center_x - 0.5, 24.2);
+        ferrule.line_to(brush_center_x + 3.8, 27.5);
+        ferrule.line_to(brush_center_x + 6.8, 23.2);
+        ferrule.line_to(brush_center_x + 2.8, 20.0);
+        ferrule.close();
+        if let Some(path) = ferrule.finish() {
+            fill_skia_path(&mut pixmap, &path, [244, 248, 255, 246]);
+            stroke_skia_path(&mut pixmap, &path, [240, 246, 255, 246], 1.2);
+        }
+        let mut bristle = tiny_skia::PathBuilder::new();
+        bristle.move_to(brush_center_x + 2.6, 20.0);
+        bristle.line_to(brush_center_x + 9.2, 15.8);
+        bristle.line_to(brush_center_x + 7.0, 11.8);
+        bristle.line_to(brush_center_x + 0.4, 16.2);
+        bristle.close();
+        if let Some(path) = bristle.finish() {
             fill_skia_path(&mut pixmap, &path, [240, 246, 255, 246]);
         }
 
@@ -11606,31 +11610,21 @@ mod windows_overlay {
         );
         draw_skia_line(
             &mut pixmap,
-            SCREEN_DRAW_TOOLBAR_ARROW_X as f32 + 7.0,
-            36.0,
-            SCREEN_DRAW_TOOLBAR_ARROW_X as f32 + 21.0,
-            20.0,
-            [240, 246, 255, 246],
-            2.2,
-        );
-        draw_skia_line(
-            &mut pixmap,
-            SCREEN_DRAW_TOOLBAR_ARROW_X as f32 + 12.5,
-            19.0,
-            SCREEN_DRAW_TOOLBAR_ARROW_X as f32 + 21.0,
-            20.0,
+            SCREEN_DRAW_TOOLBAR_ARROW_X as f32 + 6.5,
+            35.5,
+            SCREEN_DRAW_TOOLBAR_ARROW_X as f32 + 22.0,
+            19.5,
             [240, 246, 255, 246],
             2.8,
         );
-        draw_skia_line(
-            &mut pixmap,
-            SCREEN_DRAW_TOOLBAR_ARROW_X as f32 + 21.0,
-            20.0,
-            SCREEN_DRAW_TOOLBAR_ARROW_X as f32 + 17.0,
-            28.0,
-            [240, 246, 255, 246],
-            2.8,
-        );
+        let mut arrow_head = tiny_skia::PathBuilder::new();
+        arrow_head.move_to(SCREEN_DRAW_TOOLBAR_ARROW_X as f32 + 22.0, 19.5);
+        arrow_head.line_to(SCREEN_DRAW_TOOLBAR_ARROW_X as f32 + 12.6, 20.2);
+        arrow_head.line_to(SCREEN_DRAW_TOOLBAR_ARROW_X as f32 + 18.4, 29.0);
+        arrow_head.close();
+        if let Some(path) = arrow_head.finish() {
+            fill_skia_path(&mut pixmap, &path, [240, 246, 255, 246]);
+        }
 
         let rect_button_x = SCREEN_DRAW_TOOLBAR_RECT_X as f32;
         fill_skia_rounded_rect(
